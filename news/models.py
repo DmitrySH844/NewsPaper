d18b5496f9505e85a6958f2fce7efdd0562554aa
Name <email@example.ru>
2023-07-18 21:20:45 +0500

    add

--- news/models.py
+++ news/models.py
@@ -111,16 +111,6 @@ class Subscribers(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, )
 
 
-class Appointment(models.Model):
-    date = models.DateField(
-        default=datetime.utcnow,
-    )
-    client_name = models.CharField(
-        max_length=200
-    )
-    message = models.TextField()
 
-    def __str__(self):
-        return f'{self.client_name}: {self.message}'
 
 # Create your models here.
